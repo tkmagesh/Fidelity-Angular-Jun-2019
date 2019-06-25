70ec23f2b782e0b6bea214bb1f3d9da55005c823
Magesh <tkmagesh77@gmail.com>
2019-06-25 15:12:27 +0530

    bugTracker - adding and listing (Bug as model)

--- bug-tracker-app/src/app/app.module.ts
+++ bug-tracker-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BugTrackerComponent } from './bugTracker/bugTracker.component';
 
 @NgModule({
   declarations: [
     AppComponent
+    , BugTrackerComponent
   ],
   imports: [
     BrowserModule
---
+++ bug-tracker-app/src/app/bugTracker/bugTracker.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Bug } from './models/Bug';
+
+@Component({
+	selector : 'app-bug-tracker',
+	templateUrl : 'bugTracker.component.html'
+})
+export class BugTrackerComponent{
+	bugs : Bug[] = [];
+
+	onAddNewClick(newBugName : string){
+		let newBug : Bug = {
+			name : newBugName,
+			isClosed : false
+		};
+		this.bugs.push(newBug);
+	}
+}
